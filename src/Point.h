36bbeaee9bf35fde0904b1eaea299da9fd2bd451
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2016-01-19 20:09:33 +0600

    Add point and size classes

---
+++ src/Point.h
@@ -0,0 +1,17 @@
+#ifndef AURIGMA_POINT_H
+#define AURIGMA_POINT_H
+
+
+class Point
+{
+public:
+	const int X{0};
+	const int Y{0};
+
+	Point(int x = 0, int y = 0):
+		X(x),
+		Y(y) {}
+};
+
+
+#endif // AURIGMA_POINT_H
---
+++ src/Size.h
@@ -0,0 +1,17 @@
+#ifndef AURIGMA_SIZE_H
+#define AURIGMA_SIZE_H
+
+
+class Size
+{
+public:
+	const unsigned int Width{0};
+	const unsigned int Height{0};
+
+	Size(unsigned int width = 0, unsigned int height = 0):
+		Width(width),
+		Height(height) {}
+};
+
+
+#endif // AURIGMA_SIZE_H
